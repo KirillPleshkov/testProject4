e9d12280165d00e9335926d6893aea948400cefa
kirill <kirypleshkov@mail.ru>
2024-08-31 15:27:14 +0700

    Add order buy view

--- buy/urls.py
+++ buy/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from buy.views import CreateCheckoutSessionView, CancelView, SuccessView
+from buy.views import BuyItemView, BuyOrderView, CancelView, SuccessView
 
 urlpatterns = [
     path(
         "<int:item_id>/",
-        CreateCheckoutSessionView.as_view(),
-        name="create_checkout_session",
+        BuyItemView.as_view(),
+        name="buy-item",
+    ),
+    path(
+        "order/<int:order_id>/",
+        BuyOrderView.as_view(),
+        name="buy-order",
     ),
     path("cancel/", CancelView.as_view(), name="cancel"),
     path("success/", SuccessView.as_view(), name="success"),
--- buy/views.py
+++ buy/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import stripe
+from django.core.exceptions import BadRequest
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views import View
@@ -8,12 +9,13 @@ from django.conf import settings
 from django.views.generic import TemplateView
 
 from item.models import Item
+from order.models import Order
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 domain = settings.DOMAIN
 
 
-class CreateCheckoutSessionView(View):
+class BuyItemView(View):
     def get(self, request, *args, item_id: int, **kwargs) -> JsonResponse:
         item = get_object_or_404(Item, id=item_id)
 
@@ -30,6 +32,33 @@ class CreateCheckoutSessionView(View):
                 },
             ],
             metadata={"item_id": item_id},
+            mode="payment",
+            success_url=domain + "/buy/success/",
+            cancel_url=domain + "/buy/cancel/",
+        )
+        return JsonResponse({"id": checkout_session.id})
+
+
+class BuyOrderView(View):
+    def get(self, request, *args, order_id: int, **kwargs) -> JsonResponse:
+        order = get_object_or_404(Order.objects.prefetch_related("items"), id=order_id)
+
+        items_to_stripe = [
+            {
+                "price_data": {
+                    "currency": "rub",
+                    "unit_amount": int(m2m_item.item.price * 100),
+                    "product_data": {"name": m2m_item.item.name},
+                },
+                "quantity": m2m_item.count,
+            }
+            for m2m_item in order.orderitemm2m_set.all()
+        ]
+
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=items_to_stripe,
+            metadata={"order_id": order_id},
             mode="payment",
             success_url=domain + "/buy/success/",
             cancel_url=domain + "/buy/cancel/",
